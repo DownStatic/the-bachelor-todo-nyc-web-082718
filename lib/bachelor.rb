771d0388052a3cce8e706b5f7380e1b1de1128c4
Christopher Stull <chris.stull@gmail.com>
2018-08-16 20:34:14 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -49,7 +49,7 @@ def get_occupation(data, hometown)
 end
 
 def get_average_age_for_season(data, season)
-  average_age = -1
+  average_age = 0
   running_total = 0
   data.each do |which,contestants|
     if which == season
